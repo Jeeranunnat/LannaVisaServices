21ade4dae67a1465344c08e38d20a1c8f488cb59
Jeeranunnat <80268940+Jeeranunnat@users.noreply.github.com>
2023-06-22 21:29:34 +0700

    update css

--- main.js
+++ main.js
@@ -5,19 +5,13 @@ $(document).ready(function () {
     } else {
       $('nav').removeClass('sticky');
     }
-    // if(this.scrollY > 500){
-    //     $('.scroll-up-btn').addClass("show")
-    // }
-    // else{
-    //     $('.scroll-up-btn').removeClass("show")
-    // }
   });
-  // slide-up-script
-  // $('.scroll-up-btn').click(function(){
-  //     $('html').animate({scrollTop:0});
-  // })
-  // // toggle menu/navbar script
-  // $('.menu-btn').click(function(){
-  //     $('.navbar .menu').toggleClass("active");
-  //     $('.menu-btn i').toggleClass("active");
+  const menuHamburger = document.querySelector('.menu-hamburger');
+  const navLinks = document.querySelector('.nav-links');
+  menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+  });
+  navLinks.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+  });
 });
